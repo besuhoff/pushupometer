cfe4a5ca22060cf88cd1a9017bce5c0ff05bcbbe
Albert Besuhoff <albert.besuhoff@gmail.com>
2015-06-13 12:04:14 +0300

    Fix config for openshift

--- config.defaults.js
+++ config.defaults.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dbConnectParams: process.env.OPENSHIFT_NODEJS_PUSHUPOMETER_DBCONNECTPARAMS,
+  dbConnectParams: process.env.OPENSHIFT_MYSQL_DB_URL + process.env.OPENSHIFT_APP_NAME,
   clientId: process.env.OPENSHIFT_NODEJS_PUSHUPOMETER_CLIENTID,
   clientSecret: process.env.OPENSHIFT_NODEJS_PUSHUPOMETER_CLIENTSECRET
 };
