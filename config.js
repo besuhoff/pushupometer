13cff4acdf63595824c9a8fe95a01294e04cbb38
Albert Besuhoff <albert.besuhoff@gmail.com>
2015-06-13 12:30:15 +0300

    Remove config (wtf?)

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dbConnectParams: 'mysql://pushupometer:pushupometer@localhost/pushupometer',
-  clientId: '6bec491a25c0ed7045cf',
-  clientSecret: '36fa743e4fe00d65a0564150c8d1a26577d06fd4'
-};
